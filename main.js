a9d7c218b3c7e765976d70f14ec7cd37ed3d4b48
AH528ph <108016078+AH528ph@users.noreply.github.com>
2022-06-27 16:44:20 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+console .log("dhaka");
+console.log(0,12345,678,90);
+console.log(10+5);
+console.log(10-5);
+console.log(10*5);
+console.log(10/5);
+console.log(15%3);
+console.log(15%15);
+console .log(30<14);
+console .log(30<50);
+console .log(2>6);
+console .log(x=10-5);
+var x=10 ;
+console .log(x);
+var y=0 ;
+console .log(y);
+var text ="This is my site ";
+console .log(text);
+var x= "Bngladesh India Sri Langka Pakistan";
+console .log(x);
+const pie=20-60;
+let m=12-6 ;
+const n=12-6;
+console .log(pie)
+console .log(m);
+function sum(number1, number2){
+    var result=number1+number2 ;
+    console.log(result);
+}
+sum(9,8);
+sum(570,430);
+sum(5720,4380);
+sum(10,20);
+
+function great(){
+    console.log("Welcome")
+}
+great()
+great()
+var mark=94 ;
+if(mark>= 80){
+    console.log("A+");
+} else{
+    console.log("No");
+}
+var subject="math" ;
+var mark= 87 ;
+if(subject==="math" && mark >= 90){
+    console .log("A+");
+}else if(subject ="math" && mark < 90){
+    console.log("failed");
+}
+
+var subject="math" ;
+var mark = 81 ; 
+if(subject='math' && mark >= 90){
+    console .log("A+ in math");
+} else if(subject !='math' && mark >=80){
+console .log("A+ in other subject");
+} else{
+    console .log("failed");
+}
+
+function check_result(subject,mark){
+if(subject='math' && mark >= 90){
+    console .log("A+ in math");
+} else if(subject !='math' && mark >=80){
+console .log("A+ in other subject");
+} else{
+    console .log("failed");
+}
+}
+check_result('math' , 91)
+check_result('Bangla' ,87 )
+
+var number = 251 ;
+var r = number % 2 ;
+ if(r === 0){
+    console .log("Even");
+} 
+else if (r === 1){
+    console .log("odd");
+}
+var Array=[20,30,40,50,60,"Bangladesh", "India", "SriLanka"];
+console .log(Array[2]*Array[3],Array[5]);
+
+
+var Array=[20,30,40,50,60,"Bangladesh", "India", "SriLanka"];
+console .log("array");
+
+for(let i = 0; i < 10; i ++){
+    console .log(i+1);
+}
+let i=0 ;
+for(i; i <= 10; i = i+1){
+    console .log( i );
+}
